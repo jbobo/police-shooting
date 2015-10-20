9c2790b1539aad4ff0d39ce34147d450c5625bde
James Ned Bobo <harleyzone1@gmail.com>
2015-10-19 21:41:48 -0700

    Oct 19 2015 9:41 PM

--- js/map.js
+++ js/map.js
@@ -1,8 +1,10 @@
 // Function to draw your map
+var map;
+
 var drawMap = function() {
 
   // Create map and set view
- 	var map = L.map('container').setView([39, 15028], 4)
+ 	 map = L.map('container').setView([39, 15028], 4);
 	// [39, 15028], 4
 	// [latitude, longitude], zoom
 
@@ -45,6 +47,7 @@ var getData = function() {
 
 // Loop through your data and add the appropriate layers and points
 var customBuild = function(data) {
+	
 	console.log(data);
 	// Be sure to add each layer to the map
 	// var circle = new L.circleMarker([latitude, longitude], options)
@@ -53,13 +56,12 @@ var customBuild = function(data) {
 	var unarmed = new L.LayerGroup([])
 
 	data.forEach(function(d) {
-		if(d["Victim's Age"] <18) {
-			var color = "blue"
+		if(d["Victim's Age"] < 18) {
+			var color = 'blue'
 		}else{
-			var color = "red"
+			var color = 'red'
 		}
-		var circle = new L.CircleMarker([d.lat, d.lng], {fill:color,
-		})
+		var circle = new L.CircleMarker([d.lat, d.lng], {fillColor:color, radius:10,})
 		circle.bindPopup('<strong>City: </strong>' + d.City + '<br><strong>State: </strong>'+d.State+'<br><strong>Name: </strong>' + d["Victim Name"] + '<br><strong>Age: </strong>' + d["Victim's Age"] + '<br><strong>Gender: </strong>' + d["Victim's Gender"] + '<br><strong>Weapon Used: </strong>' + d["Weapon"] + '<br><strong>Date of Altercation: </strong>' + d["Timestamp"] + '<br><strong>Summary of Altercation: </strong>' + d["Summary"] + '<br><strong>Result of Altercation: </strong>' + d["Hit or Killed?"] + '<br><strong></strong>' + d["Victim's Age"])
 		if(d["Weapon"] = "Unarmed") {
 			circle.addTo(unarmed)	
@@ -73,6 +75,7 @@ var customBuild = function(data) {
     		"Armed Suspects": armed,
 		"Unarmed Suspects": unarmed
 	};
+
 	L.control.layers(null,armed, unarmed).addTo(map);
 
 }
